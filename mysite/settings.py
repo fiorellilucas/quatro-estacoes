b8c8cdcf1c11d8a1d70a21208b90302c31c55d58
Lucas Fiorelli <fiorellilucas@gmail.com>
2022-04-29 13:42:13 -0300

    Adicionar Procfile e runtime.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, os.getenv("STATIC_ROOT"))
+STATIC_ROOT = BASE_DIR / os.getenv("STATIC_ROOT")
 STATIC_URL = 'static/'
 
 # Default primary key field type
