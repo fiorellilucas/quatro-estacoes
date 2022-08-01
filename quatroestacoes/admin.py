89ec7c418388703cb90ec5c89cf3635962c88bf9
Lucas Fiorelli <fiorellilucas@gmail.com>
2022-08-01 09:08:46 -0300

    Adicionar Reclamações e Reservas à dashboard do admin

--- quatroestacoes/admin.py
+++ quatroestacoes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Morador, Aviso, Reuniao
+from .models import Morador, Aviso, Reuniao, Reclamacao, Reserva
 
-admin.site.register([Morador, Aviso, Reuniao])
+admin.site.register([Morador, Aviso, Reuniao, Reclamacao, Reserva])
 
