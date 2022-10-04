97a467c46042f7eb92134412c84ed9da86835dce
Lucas Fiorelli <fiorellilucas@gmail.com>
2022-10-04 01:35:29 -0300

    Apagar código e templates que não serão mais utilizadas

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,18 +1,3 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,8 +6,4 @@ from quatroestacoes import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('quatroestacoes.urls')),
-    path("senha/", views.MudarSenhaView.as_view(), name="password_reset"),
-    path("senha/pronto", views.MudarSenhaPronto.as_view(), name="password_reset_done"),
-    path("senha/confirmar/<uidb64>/<token>", views.MudarSenhaConfirmar.as_view(), name="password_reset_confirm"),
-    path("senha/completar", views.MudarSenhaCompletar.as_view(), name="password_reset_complete"),   
 ]
